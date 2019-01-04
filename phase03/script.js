acffcc8c9684f10ed1deb04bc35422f5dd266239
ldu2 <ldu2@binghamton.edu>
2019-01-04 09:28:14 -0500

    Initialize phase03

---
+++ phase03/script.js
@@ -0,0 +1,69 @@
+$(function() {
+	$("#inputText").val('');
+});
+
+function addElement(){
+	if($("#inputText").val() ==''){
+		alert("Task cannot be empty");
+		return true;
+	}
+
+	//get the table and create the row from the small
+	//to big
+	let table = $("#todoList");
+	let row = $("<tr></tr>");
+	let left = $("<td></td>");
+	let clabel = $("<label class='container'></label>");
+	let cbox = $("<input type='checkbox'></input>");
+	let cmark = $("<span class='checkmark'></span>");
+
+	//set attribute and wrap it up and get it to the left
+	clabel.text($('#inputText').val());
+	clabel.click(function() {
+	    if(clabel.attr('class')=='container'){
+	    	clabel.toggleClass('checked');
+	    }
+	    else{
+		clabel.attr('class','container');
+	    }
+	});
+
+	clabel.append(cbox);
+	clabel.append(cmark);
+	left.append(clabel);
+	row.append(left);
+	
+	//now the right hand side elements
+	let right = $("<td></td>");
+	let chunk = $("<div style='color:#91C6D9'></div>");
+	let editButton = $("<button class = 'astext' id='editBtn'>edit</button>").click(function () {
+			let task = prompt("New Task", clabel.text());
+			if(task!=null){
+				clabel.text(task);
+				clabel.append(cbox);
+				clabel.append(cmark);
+			}
+		}
+	);
+	let sepChar = " | ";
+	let deleteButton = $("<button class = 'astext' id='delBtn'>delete</button>").click(function(){
+			$("#delBtn").parent().parent().parent().css("display","none");
+		}
+	);
+	//stack them together and wrap them up
+
+	chunk.append(editButton);
+	chunk.append(sepChar);
+	chunk.append(deleteButton);
+	right.append(chunk);
+	//finally put them there
+	row.append(right);
+	table.append(row);
+	
+	$("#inputText").val('');
+}
+
+function addElementEnter(e){
+	if(e.keyCode == 13)
+		addElement();
+}
